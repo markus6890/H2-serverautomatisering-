278959bfa13541371c7159cbc6951423942ed8e2
markus6890 <markushygedombrowski@gmail.com>
2025-05-13 09:44:45 +0200

    First commit

---
+++ H2 project serautomatisering/powershell scripts/script1.ps1
@@ -0,0 +1,3 @@
+$Path = "C:\Users\Bruger\Documents\GitHub\H2-serverautomatisering-";
+New-Item -Path $Path -Name "oprettetfil.txt" -ItemType "File" -Value "Jeg har tilføjet text i filen da den blev oprettet";
+
