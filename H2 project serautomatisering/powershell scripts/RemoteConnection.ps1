9bc5632bc862955431ee34dd8e739d7e42296f5a
markus6890 <markushygedombrowski@gmail.com>
2025-05-14 10:45:48 +0200

    AD Script please

---
+++ H2 project serautomatisering/powershell scripts/RemoteConnection.ps1
@@ -0,0 +1,54 @@
+
+function GetUsersFromCSV{
+    Import-Module ActiveDirectory
+
+    $ADUsers = Import-Csv "C:\Users\krizo\Serverautomatisering\testemployees1.CSV" -Delimiter ";"
+    $UPN = "Markus.ninja"
+    $ADPath = ",OU=Dystopian Tech,DC=Markus,DC=ninja";
+
+    foreach ($User in $ADUsers) {
+        try {
+            $ADUserPath = "$($User.ou)$($ADPath)";
+            if(Get-ADOrganizationalUnit -Filter "distinguishedName -ne $($ADUserPath)") {
+                Write-Host("Creating OU")
+                New-ADOrganizationalUnit -Name "$($User.ou)" -Path "$($ADPath)";
+                Write-Host("OU created: " + $ADUserPath);
+            }
+            $UserParams = @{
+                SamAccountName      = $User.username
+                UserprincipalName   = "$($User.username)@$UPN"
+                Name                = "$($User.firstname) $($User.lastname)"
+                GivenName           = $User.firstname
+                Surname             = $User.lastname
+                Initial             = $User.initials
+                Enabled             = $true
+                DisplayName         = "$($User.firstname) $($User.lastname)"
+                Path                = $ADUserPath
+                City                = $User.city
+                PostalCode          = $Uder.zipcode
+                Country             = $User.country
+                Company             = $User.company
+                State               = $User.state
+                StreetAddress       = $User.streetaddress
+                OfficePhone         = $User.telephone
+                EmailAddress        = $User.email
+                Title               = $User.jobtitle
+                Department          = $User.department
+                AccountPassword     = (ConvertTo-SecureString $User.password -AsPlainText -Force)
+                ChangePasswordAtLogon = $true
+            }
+
+            if (Get-ADUser -Filter "SamAccountName -eq '$($User.username)'") {
+                Write-Host "A user with username $($User.username) already exists in Active Directory." -ForegroundColor Magenta;
+            }
+            else {
+                New-ADUser @UserParams
+                Write-Host "The user $($User.username) is created." -ForegroundColor Green
+            }
+        }
+        catch {
+            Write-Host "Failed to create user $($User.username) - $_" -ForegroundColor Red
+        }
+    }
+
+}
--- H2 project serautomatisering/powershell scripts/script1.ps1
+++ H2 project serautomatisering/powershell scripts/script1.ps1
@@ -1,3 +1,85 @@
-$Path = "C:\Users\Bruger\Documents\GitHub\H2-serverautomatisering-";
-New-Item -Path $Path -Name "oprettetfil.txt" -ItemType "File" -Value "Jeg har tilføjet text i filen da den blev oprettet";
+# PowerShell opgaver
+# opgave 1
+function opg1
+{
+    $Path = "C:\Users\Bruger\Documents\GitHub\H2-serverautomatisering-";
+    New-Item -Path $Path -Name "oprettetfil.txt" -ItemType "File" -Value "Jeg har tilføjet text i filen da den blev oprettet" -WhatIf;
+}
 
+# opgave 2
+function opg2
+{
+    Get-Process;
+
+}
+# opgave 3
+function opg3
+{
+    $Password = Read-Host -AsSecureString;
+    $usersettings = @{
+        Name = 'Maya'
+        Password = $Password
+        FullName = 'Maya blob trorsen'
+        Description = 'Accounnnnnt'
+    }
+    New-LocalUser @usersettings;
+
+}
+
+# opgave 4
+function opg4
+{
+    robocopy C:\Users\Bruger\Documents\GitHub\H2-serverautomatisering- "C:\Users\Bruger\Documents\GitHub\H2-serverautomatisering-\backup" oprettetfil.txt /z
+
+}
+
+#opgave 5
+function opg5
+{
+    Get-NetTCPConnection
+}
+
+#opgave 6
+function opg6
+{
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser;
+    Get-ExecutionPolicy -List
+}
+opg6;
+
+#opgave 7
+function opg7
+{
+
+}
+
+#opgave 8
+function opg8
+{
+    Get-AppxPackage;
+}
+
+#opgave 9
+function opg9
+{
+    Get-EventLog -LogName System -EntryType Error;
+}
+
+#opgave 10
+function opg10 {
+    Start-Transcript -Path "C:\Users\Bruger\Documents\GitHub\H2-serverautomatisering-\Updatelog.txt" -Append
+
+    try {
+        winget upgrade --id Notepad++.Notepad++ --accept-source-agreements --accept-package-agreements
+    } catch {
+        Write-Output "Opdatering fejlede: $_"
+    }
+
+    Stop-Transcript
+
+}
+
+function opg11
+{
+
+}
