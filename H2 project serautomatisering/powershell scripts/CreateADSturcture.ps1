6e4ac61a9eca7fcacf696a7927985ffdf22d31e1
markus6890 <markushygedombrowski@gmail.com>
2025-05-19 09:02:24 +0200

    small security update

--- H2 project serautomatisering/powershell scripts/CreateADSturcture.ps1
+++ H2 project serautomatisering/powershell scripts/CreateADSturcture.ps1
@@ -1,5 +1,6 @@
 $cred = Get-Credential
-$session = New-PSSession -ComputerName "10.14.2.56" -Port 4335 -Credential $cred -Authentication Default -UseSSL:$false
+$serverIP = Read-Host("Enter the server IP address")
+$session = New-PSSession -ComputerName $serverIP -Port 4335 -Credential $cred -Authentication Default -UseSSL:$false
 
 Invoke-Command -Session $session -ScriptBlock {
 
